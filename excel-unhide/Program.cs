38bd9c7ada33cbea9106c55902b440fde54c5bc7
Nick Stein <nick@nickstein.net>
2014-10-16 22:21:33 -0400

    Initial commit

---
+++ excel-unhide/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace excel_unhide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Excel.Application app = (Excel.Application) Marshal.GetActiveObject("Excel.Application");
+
+                foreach (Excel.Worksheet worksheet in app.Worksheets)
+                {
+                    worksheet.Visible = Excel.XlSheetVisibility.xlSheetVisible;
+                }
+            }
+
+            catch (Exception e) { Console.WriteLine(String.Format("Error unhiding sheets. Exception details: {0}", e.Message)); }
+        }
+    }
+}
